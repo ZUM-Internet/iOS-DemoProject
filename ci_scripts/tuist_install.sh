1464bf7a506b61c3a8f9321eff8981bbe8c44519
siwonkim <siwonkim@zuminternet.com>
2024-01-08 17:30:15 +0900

    :recycle: script 경로추가 6

--- ci_scripts/tuist_install.sh
+++ ci_scripts/tuist_install.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
-#brew install mise
-
-#mise install tuist@3.38.0
-#mise use tuist@3.38.0
-#mise plugins
-#mise ls tuist
-
-#$CI_PROJECT_FILE_PATH
-#echo $CI_PROJECT_FILE_PATH
-#echo $PATH
-
-#$CI_ARCHIVE_PATH
-#echo $CI_ARCHIVE_PATH
+brew install mise
 
 echo "tuist install..."
-curl -Ls https://install.tuist.io | bash
+mise install tuist@3.38.0
+mise use tuist@3.38.0
+mise plugins
+mise ls tuist
+
+
+#curl -Ls https://install.tuist.io | bash
 
 echo "tuist generate..."
 
