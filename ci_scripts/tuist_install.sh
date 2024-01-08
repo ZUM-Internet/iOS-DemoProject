b278829772fe6b373c45be1b045d8d6202bab189
siwonkim <siwonkim@zuminternet.com>
2024-01-08 15:50:57 +0900

    :recycle: script homebrew로 수정

--- ci_scripts/tuist_install.sh
+++ ci_scripts/tuist_install.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
-echo "mise install..."
-curl https://mise.jdx.dev/install.sh | sh
-eval "$(/Users/local/.local/bin/mise activate zsh)" >> "/Users/local/.zshrc"
-mise doctor
 
-echo "tuist install..."
-mise install tuist@3.38.0
-mise use tuist@3.38.0
-
-pwd
-
+brew install tuist
 echo "tuist generate..."
-#tuist clean --path ..
-#tuist fetch --path ..
-~/.local/share/mise/plugins/tuist/bin/tuist fetch
-~/.local/share/mise/plugins/tuist/bin/tuist generate
+tuist clean --path ..
+tuist fetch --path ..
+tuist generate --path ..
 
