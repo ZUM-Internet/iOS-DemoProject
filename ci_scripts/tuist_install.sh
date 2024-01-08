142fe74684edbec5e020292bc41fdf46f0808e2a
siwonkim <siwonkim@zuminternet.com>
2024-01-08 14:33:33 +0900

    :recycle: script 수정 7

--- ci_scripts/tuist_install.sh
+++ ci_scripts/tuist_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "mise install..."
 curl https://mise.jdx.dev/install.sh | sh
-echo "eval \"\$(/Users/local/.local/bin/mise activate zsh)\"" >> "/Users/local/.zshrc"
+eval "$(/Users/local/.local/bin/mise activate zsh)" >> "/Users/local/.zshrc"
 mise doctor
 
 echo "tuist install..."
