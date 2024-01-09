907f1407edd255305ae25cfd6247acfdd1ef8727
siwonkim <siwonkim@zuminternet.com>
2024-01-09 09:29:08 +0900

    :recycle: script 정리

--- ci_scripts/tuist_install.sh
+++ ci_scripts/tuist_install.sh
@@ -4,15 +4,9 @@ brew install mise
 echo "tuist install..."
 mise install -g tuist@3.38.0
 tuist_path=~/.local/share/mise/shims/tuist
-mise use tuist@3.38.0
-mise plugins
 mise ls tuist
 
-
-#curl -Ls https://install.tuist.io | bash
-
 echo "tuist generate..."
-
 $tuist_path clean --path ..
 $tuist_path fetch --path ..
 $tuist_path generate --path ..
